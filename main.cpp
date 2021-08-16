b2212ed6d98af772aa773fccf989cf81a8ef532e
ernikus <ernikus2000@gmail.com>
2021-08-16 16:32:27 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,118 +1,217 @@
-#include <iostream>
-#include <stack>
-#include <string>
-#include <cstring>
-#include <set>
+﻿#include <iostream> //input-output operations
+#include <vector>   //vectors
+#include <stack>    //stacks
+#include <string>   //stod (string to double) operations
 
-/* void plus_and_minus(stack& stosLiczb, const char* lel)
+
+void to_vector(const std::string orgData, std::vector<std::string> &divData)
 {
-    char a = stosLiczb.top();
-    cout << a << " ";
-    stosLiczb.pop();
-    stosLiczb.push(x[i]);
-} */
-
-int main()
-{
-    std::string inputBuffer;
-    std::string sym;
-    std::cout << "Program Odwortna Notacja Polska" << std::endl;
-    std::cout << "Wpisz dzialanie do przekonwertowania na ONP: ";
-    std::cin >> inputBuffer;
-    std::stack <int> stosLiczb;
-    std::cout << "Wprowadziles " << inputBuffer.size() << " znakow!" << std::endl;
-
-    /* std::set <char> signs = {+, -, *, /, ^, (, )}; */
-
-    for(int i=0; i<=inputBuffer.size(); i++)
+    std::vector<std::string> result{};
+    std::string signData{};
+    
+    
+    for (int i = 0; i < orgData.size(); ++i)
     {
-        if(inputBuffer[i]=='+'||inputBuffer[i]=='-'||inputBuffer[i]=='*'||inputBuffer[i]=='/'||
-           inputBuffer[i]=='^'||inputBuffer[i]=='('||inputBuffer[i]==')')
+        if (!isdigit(orgData[i]))
         {
-            std::cout << " ";
-            if(stosLiczb.empty()==true)
-            {
-                stosLiczb.push(inputBuffer[i]);
-            }
-            else if(stosLiczb.top()=='^')
-            {
-                char a = stosLiczb.top();
-                std::cout << a << " ";
-                stosLiczb.pop();
-                if(stosLiczb.top()=='*'||stosLiczb.top()=='/')
-                {
-                    char a = stosLiczb.top();
-                    std::cout << a << " ";
-                    stosLiczb.pop();
-                    stosLiczb.push(inputBuffer[i]);
-                }
-                else
-                {
-                    stosLiczb.push(inputBuffer[i]);
-                }
-            }
-            else if(inputBuffer[i]=='('||inputBuffer[i]==')')
-            {
-                std::cout << '\b';
-                stosLiczb.push(inputBuffer[i]);
-            }
-            else if((inputBuffer[i]=='+'||inputBuffer[i]=='-')&&(stosLiczb.top()=='*'||stosLiczb.top()=='/'))
-            {
-                char a = stosLiczb.top();
-                std::cout << a << " ";
-                stosLiczb.pop();
-                if((inputBuffer[i]=='+'||inputBuffer[i]=='-')&&(stosLiczb.top()=='+'||stosLiczb.top()=='-'))
-                {
-                    char a = stosLiczb.top();
-                    std::cout << a << " ";
-                    stosLiczb.pop();
-                    stosLiczb.push(inputBuffer[i]);
-                }
-                else
-                {
-                    std::cout << inputBuffer[i] << " ";
-                }
-            }
-            else if((inputBuffer[i]=='+'||inputBuffer[i]=='-')&&(stosLiczb.top()=='+'||stosLiczb.top()=='-'))
-            {
-                char a = stosLiczb.top();
-                std::cout << a << " ";
-                stosLiczb.pop();
-                stosLiczb.push(inputBuffer[i]);
-            }
-            else if((inputBuffer[i]=='*'||inputBuffer[i]=='/')&&(stosLiczb.top()=='*'||stosLiczb.top()=='/'))
-            {
-                char a = stosLiczb.top();
-                std::cout << a << " ";
-                stosLiczb.pop();
-                stosLiczb.push(inputBuffer[i]);
-            }
-            else
-            {
-                stosLiczb.push(inputBuffer[i]);
-            }
+            if(!signData.empty())
+                result.push_back(signData);
 
-        }
-        else if(isdigit(inputBuffer[i]))
-                {
-                    std::cout << inputBuffer[i];
-                }
-    }
-    std::cout << " ";
-    while(stosLiczb.empty()==false)
-    {
-        char a = stosLiczb.top();
-        if(a=='('||a==')')
-        {
-            stosLiczb.pop();
+            signData = orgData[i];
+            result.push_back(signData);
+            signData.erase();
         }
         else
         {
-            std::cout << a << " ";
-            stosLiczb.pop();
+            signData += orgData[i];
         }
     }
-    std::cout << std::endl;
 
-    return 0;
+    if(!signData.empty())
+        result.push_back(signData);
+
+    divData = result;
+}
+
+int numChar(const char character)
+{
+    if (character == '+' || character == '-')
+        return 1;
+    else if (character == '*' || character == '/')
+        return 2;
+    else if (character == '^')
+        return 3;
+    else
+        return 0;
+}
+
+void conventer(std::vector<std::string> &divData)
+{
+    std::stack <char> stos{};
+    std::vector<std::string> Data{};
+
+
+    for (int i = 0; i < divData.size(); ++i)
+    {
+        std::string trial{ divData[i] };
+
+        if (!isdigit(trial.front()))
+        {
+            if (stos.empty())
+            {
+                stos.push(trial.front());
+                continue;
+            }
+            
+            int num1 = numChar(trial.front());
+            int num2 = numChar(stos.top());
+
+            if (trial.front() == '(')
+            {
+                stos.push(trial.front());
+            }
+            else if (trial.front() == ')')
+            {
+                while (stos.top() != '(')
+                {
+                    std::string trial2{ stos.top() };
+                    Data.push_back(trial2);
+                    stos.pop();
+                }
+
+                //remove '('
+                stos.pop();
+            }
+            else if (num1 < num2)
+            {
+                while (num1 <= num2)
+                {
+                    std::string trial2{ stos.top() };
+                    Data.push_back(trial2);
+                    stos.pop();
+
+                    if (stos.empty())
+                        break;
+
+                    num2 = numChar(stos.top());
+                }
+
+                //add to the stack new char
+                stos.push(trial.front());
+            }
+            else if (num1 == num2)
+            {
+                //remove old
+                std::string trial2{ stos.top() };
+                Data.push_back(trial2);
+                stos.pop();
+
+                //add to the stack new char
+                stos.push(trial.front());
+            }
+            else
+            {
+                stos.push(trial.front());
+            }
+        }
+        else
+        {
+            Data.push_back(divData[i]);
+        }
+    }
+
+
+    while (!stos.empty())
+    {
+        std::string trial2{ stos.top() };
+        Data.push_back(trial2);
+        stos.pop();
+    }
+        
+    divData = Data;
+
+    for (int i = 0; i < Data.size(); ++i)
+    {
+        std::cout << Data[i] << " ";
+    }
+    std::cout << std::endl;
+}
+
+double counter(std::vector<std::string> divData)
+{
+    std::stack<std::string> stos{};
+
+    for (int i = 0; i < divData.size(); ++i)
+    {
+        std::string trial{ divData[i] };
+
+
+        if (!isdigit(trial.back()))
+        {
+            double num1 = std::stod(stos.top());
+            stos.pop();
+            double num2 = std::stod(stos.top());
+            stos.pop();
+
+            std::swap(num1, num2);
+
+            
+            if (trial.front() == '+')
+            {
+                stos.push(std::to_string(num1 + num2));
+            }
+            else if (trial.front() == '-')
+            {
+                stos.push(std::to_string(num1 - num2));
+            }
+            else if (trial.front() == '*')
+            {
+                stos.push(std::to_string(num1 * num2));
+            }
+            else if (trial.front() == '/')
+            {
+                stos.push(std::to_string(num1 / float(num2)));
+            }
+            else if (trial.front() == '^')
+            {
+                stos.push(std::to_string(pow(num1, num2)));
+            }
+            else
+                exit(-1);
+        }
+        else
+        {
+            stos.push(trial);
+        }
+    }
+    
+    
+    double result = std::stod(stos.top());
+    
+
+    if (result == int(result))
+        return int(result);
+    else
+        return result;
+}
+
+double instructions(const std::string orgData)
+{
+    std::vector<std::string> divData{};
+
+    to_vector(orgData, divData);
+    conventer(divData);
+
+    return (counter(divData));
+}
+
+
+int main()
+{
+    std::string equation{};
+
+    std::cout << "Give the equation: ";
+    std::cin >> equation;
+
+    std::cout << "The Result: " << instructions(equation) << std::endl;
 }
